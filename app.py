ebc2bf270a2f6f4f1c7423e9cf1847e6cd1f2700
Ammar <ammarjawed.1111@gmail.com>
2024-11-14 06:21:59 +0300

    Update application to add browser mic feature

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ import threading
 import time
 import streamlit as st
 from model import TranslatorModel
+from browser_app import BrowserMicrophone
 from tempfile import NamedTemporaryFile
+
+
+from audio_recorder_streamlit import audio_recorder
+
+is_brw_model = False
 
 # Set up the Streamlit app title
 st.title("Healthcare Translation App")
@@ -34,6 +40,7 @@ st.write(f"Target Language: {target_language} ({languages[target_language]})")
 
 # Buttons and file upload on the left side (column 1)
 with col1:
+    brw_record_but = audio_recorder("Record from browser")
     record_but = st.button("Start Real Time Recording")
     stop_but = st.button("Stop Real Time Recording")
     tran_but = st.button("Change Translate")
@@ -48,6 +55,9 @@ if 'model' not in st.session_state:
                                              src_lang_name=source_language, 
                                              tgt_lang_name=target_language)
 
+if 'brw_model' not in st.session_state:
+    st.session_state.brw_model = BrowserMicrophone(src_lang=languages[source_language])
+
 if 'record' not in st.session_state:
     st.session_state.record = False
 
@@ -59,8 +69,12 @@ with col2:
         st.session_state.target_placeholder = st.empty()
 
 # Check if the source or target language has changed
+if 'audio_bytes' not in st.session_state:
+    st.session_state.audio_bytes = bytearray()
+
 if 'prev_source_language' not in st.session_state:
-    st.session_state.prev_source_language = source_language
+    st.session_state.prev_source_language = target_language
+
 if 'prev_target_language' not in st.session_state:
     st.session_state.prev_target_language = target_language
 
@@ -72,15 +86,19 @@ if source_language != st.session_state.prev_source_language or target_language !
                                              src_lang_name=source_language, 
                                              tgt_lang_name=target_language)
     
+    st.session_state.brw_model = BrowserMicrophone(src_lang=languages[source_language])
+    
     # Update previous language states
     st.session_state.prev_source_language = source_language
     st.session_state.prev_target_language = target_language
 
 # Access the model stored in session state
 model = st.session_state.model
+brw_model = st.session_state.brw_model
 record = st.session_state.record
 source_placeholder = st.session_state.source_placeholder
 target_placeholder = st.session_state.target_placeholder
+audio_bytes = st.session_state.audio_bytes
 
 # Function to handle speech-to-text conversion
 def speech_to_text():
@@ -101,10 +119,10 @@ with col2:
 # Handling buttons' actions
 if record_but:
     model.STOP_LISTENING = False
-    record = True
 
     # Start speech-to-text and translation in background threads
     t1 = threading.Thread(target=speech_to_text)
+
     t2 = threading.Thread(target=translate)
     t1.start()
     t2.start()
@@ -113,12 +131,34 @@ if record_but:
     while record:
         source_text = " ".join(model.src_transcript)
         target_text = model.tgt_transcript
+        
         with col2:
             # Display updated transcripts
             source_placeholder.write(f"Source transcript: {source_text}")
             target_placeholder.write(f"Target transcript: {target_text}")
         
         time.sleep(1)  # Update every second
+
+
+# Handling buttons' actions
+if brw_record_but:
+    brw_model.STOP_LISTENING = False
+
+    def fill_buff():
+        brw_model._buff.put(brw_record_but)
+        transcript = brw_model.convert_browser(brw_record_but)
+        #audio_bytes = brw_model._buff.get()
+        #st.audio(audio_bytes, format="audio/mp3")
+        if [transcript] != [None]:
+            print("BEFOREEE:",model.src_transcript)
+            model.src_transcript.append(transcript)
+            print("AFTERRR:",model.src_transcript)
+        model.TranscriptTranslator(run_once=True)
+
+        source_placeholder.write(f"Source transcript: {' '.join(model.src_transcript)}")
+        target_placeholder.write(f"Target transcript: {model.tgt_transcript}")
+
+    fill_buff()
 
 if stop_but:
     model.STOP_LISTENING = True
